2a2f4be140f45b4bf465f0b5d256952e4a3b7271
neurongaragesale <cloudservices@neurongaragesale.com>
2021-02-24 23:57:09 -0600

    added gatsby gist
    it works manually, but timed out so need to test another day

--- _ngsdataconfig/_ngsdataconfig.psm1
+++ _ngsdataconfig/_ngsdataconfig.psm1
@@ -5,6 +5,7 @@ $windowsGeneric = "https://gist.githubusercontent.com/neurongaragesale/a7616c121
 $developerBase = "https://gist.githubusercontent.com/neurongaragesale/8580e8766b19c253c517f6ff67537d8b/raw/5e8888820bb1b27de7857ff049c086b1422d1a9c/boxstarter-windows-dev-base.txt"
 $developerDocker = "https://gist.githubusercontent.com/neurongaragesale/0603afa48c49bad37abd3e5a21cb7889/raw/8507461f2ffd284bc5f9d87049153bde69643eb9/boxstarter-windows-dev-docker"
 $cieWindows = "https://gist.githubusercontent.com/neurongaragesale/b0ef496e1f14a445e42116eadcc0232c/raw/903f2c22d18c67c44968fe40c6af28aa7de55cde/boxstarter-cie-windows.txt"
+$gatsby = "https://gist.githubusercontent.com/neurongaragesale/e4a0747f75e8b103fce0e6a5bd0cd93a/raw/1d4c931d1ba47b648a2cc0a498b047cd101a7fcc/boxstarter-windows-gatsby.txt";
 
 #endregion
 
@@ -16,6 +17,7 @@ function Get-AvailablePackages{
     Write-Host "Developer-Base: used for general development"
     Write-Host "Developer-Docker: used for developing solutions in docker"
     Write-Host "CIE-Windows: Used for int servers"
+    Write-Host "Developer-Gatsby: Setup for developing with gatsby.js"
 }
 
 function Set-MachineConfig {
@@ -29,6 +31,7 @@ function Set-MachineConfig {
         "Developer-Base" { Install-BoxstarterPackage -PackageName $developerBase  }
         "Developer-Docker" { Install-BoxstarterPackage -PackageName $developerDocker  }
         "CIE-Windows" { Install-BoxstarterPackage -PackageName $cieWindows  }
+        "Developer-Gatsby" { Install-BoxstarterPackage -PackageName $gatsby  }
         Default {
             Write-Error "Unknown config"
         }
